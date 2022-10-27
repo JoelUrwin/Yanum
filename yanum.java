13601a1379ad5662c882398a84c95369a232e5e4
Joel @ Laptop <joel@joelurw.in>
2022-10-27 16:43:22 +0100

    Began work on building a parser

---
+++ yanum.java
@@ -0,0 +1,1 @@
+enum user_passwords {
--- yanum/yanum.py
+++ yanum/yanum.py
@@ -1,8 +1,26 @@
 import yaml
-from yaml import Loader, Dumper
+from yaml import load, Loader
 
 class Yanum():
+    def __init__(self, args):
+        self.filepath = args['f']
+        self.yaml = load(self.filepath, Loader=Loader)
+        self.keys = list(self.yaml.keys())
+        self.values = list(*self.yaml.values())
+        self.dump = yaml.dump(self.yaml)
 
-    def __init__(self,args):
-        self.file = args['f']
-        self.yaml = yaml.load(self.file, Loader=Loader)
+        self.dumpfile = open("yanum.java","w")
+
+        def parser():
+            print(self.values)
+            print(self.keys)
+
+        if args['f']:
+            self.dumpfile.write(f"enum {self.keys[0]} "+"{")
+            parser()
+
+
+
+
+
+
